8f9f57134869ccd0f9aeaac6e72f9e39669aadbc
Abyss901 <70577701+Abyss901@users.noreply.github.com>
2023-05-07 14:00:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+}
+
+
+function setup() {
+canvas = createCanvas(300, 300);
+canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('RealtimeFilter.png');
+}
+
+console.log('Clown to clown communication!!!!')
